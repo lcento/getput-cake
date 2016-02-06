0a1d9c1cac592634d0d9fab5e730aae246c10d4e
Leo Cent <lcento@gmail.com>
2016-02-06 19:04:50 +0100

    My second hope

--- app/tests/string.php
+++ app/tests/string.php
@@ -11,14 +11,19 @@ class String
 
     // creates a deep copy of the string object
     function copy() {
+        $ret = new String($this->data);
+        return $ret;
     }
 
     // adds another string object to this class
     function add($string) {
+        $this->data = $this->data.$string->toString("%s");
     }
 
     // returns the formated string
     function toString($format) {
+        $ret = sprintf($format, $this->data);
+        return $ret;
     }
 }
 ?>
