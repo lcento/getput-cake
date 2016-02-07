d66a305b3860201c8aa921ab5fa687e4bfabeac7
Leo Cent <lcento@gmail.com>
2016-02-07 18:32:25 +0100

    commit 07022016 18:32

---
+++ app/config/msg.php
@@ -0,0 +1,102 @@
+<?php
+	$config = array(
+
+	/************************/
+	/* msg AdminsController */
+	/************************/
+
+        /* msg saveIfValid */
+        'msg_check_save_error' => "Errore durante l'inserimento dei dati.",
+
+        /* msg insusersOkMsg */
+        'msg_insusers_ok' => "Dati inseriti con successo.",
+
+        /* msg modusersOkMsg */
+        'msg_modusers_ok' => "Dati aggiornati con successo.",
+
+	/* msg_mnuaddon_ok */
+	'msg_mnuaddon_ok' => "Dati inseriti con successo.",
+
+	/* msg_mnuaddon_err */
+	'msg_mnuaddon_err' => "Errore durante l'inserimento dei dati.",
+
+        /* msg canusersOkMsg */
+        'msg_canusers_ok' => "Dati rimossi con successo.",
+
+        /* msg canusersErrMsg */
+        'msg_canusers_err' => "Errore durante la cancellazione dei dati.",
+
+	/* msg viewusersDisabled */
+	'msg_viewusers_disabled' => "DISABLED",
+
+	/* msg viewusersEnabled */
+	'msg_viewusers_enabled' => "ENABLED",
+
+	/************************/
+	/* msg UploadsController */
+	/************************/
+
+	/* msg articles */
+	'msg_articles_file_invalid' => "File non valido !!!",
+	'msg_articles_file_empty' => "File non selezionato !!!",
+	'msg_articles_file_size' => 'La dimensione del file non è valida !!!',
+	'msg_articles_upload_ok' => 'File valido e inviato con successo.',
+	'msg_articles_upload_error' => "Errore durante l'invio del file !!!",
+
+	/* msg photo */
+	'msg_photo_file_invalid1' => "File 1 non valido !!!",
+	'msg_photo_file_invalid2' => "File 2 non valido !!!",
+	'msg_photo_file_invalid3' => "File 3 non valido !!!",
+	'msg_photo_file_invalid4' => "File 4 non valido !!!",
+	'msg_photo_file_invalid5' => "File 5 non valido !!!",
+	'msg_photo_file_empty' => "Files non selezionati !!!",
+	'msg_photo_file_size1' => 'File 1 : La dimensione del file non è valida !!!',
+	'msg_photo_file_size2' => 'File 2 : La dimensione del file non è valida !!!',
+	'msg_photo_file_size3' => 'File 3 : La dimensione del file non è valida !!!',
+	'msg_photo_file_size4' => 'File 4 : La dimensione del file non è valida !!!',
+	'msg_photo_file_size5' => 'File 5 : La dimensione del file non è valida !!!',
+	'msg_photo_upload_error' => "Errore durante l'invio del file !!!",
+	'msg_photo_upload_ok' => 'Invio effettuato con successo.',
+
+	/* msg items */
+	'msg_items_file_invalid1' => "File 1 non valido !!!",
+	'msg_items_file_invalid2' => "File 2 non valido !!!",
+	'msg_items_file_invalid3' => "File 3 non valido !!!",
+	'msg_items_file_invalid4' => "File 4 non valido !!!",
+	'msg_items_file_invalid5' => "File 5 non valido !!!",
+	'msg_items_file_empty' => "Files non selezionati !!!",
+	'msg_items_file_size1' => 'File 1 : La dimensione del file non è valida !!!',
+	'msg_items_file_size2' => 'File 2 : La dimensione del file non è valida !!!',
+	'msg_items_file_size3' => 'File 3 : La dimensione del file non è valida !!!',
+	'msg_items_file_size4' => 'File 4 : La dimensione del file non è valida !!!',
+	'msg_items_file_size5' => 'File 5 : La dimensione del file non è valida !!!',
+	'msg_items_upload_error' =>" Errore durante l'invio del file !!!",
+	'msg_items_upload_ok' => 'Invio effettuato con successo.',
+
+	/************************/
+	/* msg UploadsController */
+	/************************/
+
+	/* msg items */
+	'msg_items_file_unselected' => "unselected",
+	'msg_items_file_selected' => "selected",
+	'msg_items_file_updated' => "updated",
+
+	/* msg download */
+	'msg_items_not_allowed' => 'Download non consentito !!!',
+
+	/***************************/
+	/* msg HistoriesController */
+	/**************************/
+
+	/* msg viewputpdf */
+	'msg_viewputpdf_not_allowed' => 'Troppi dati. Ridurre utilizzando il filtro !!!',
+
+	/* msg viewgetpdf */
+	'msg_viewgetpdf_not_allowed' => 'Troppi dati. Ridurre utilizzando il filtro !!!',
+
+	/* msg viewconpdf */
+	'msg_viewconpdf_not_allowed' => 'Troppi dati. Ridurre utilizzando il filtro !!!'
+
+        );
+?>
